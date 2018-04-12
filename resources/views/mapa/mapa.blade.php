7b999e44e1d2ac1c7a0d0ee5953c6ed79da00d2a
iedcali <nraivan3@gmail.com>
2018-04-12 15:15:38 -0400

    archivo mapa modificado

--- resources/views/mapa/mapa.blade.php
+++ resources/views/mapa/mapa.blade.php
@@ -9,13 +9,20 @@
 
 </div>
 <script>
-    var map;
     function initMap() {
-        map = new google.maps.Map(document.getElementById('map'), {
-            center: {lat: -34.397, lng: 150.644},
-            zoom: 8
-        });
-    }
+  var myLatLng = {lat: -25.363, lng: 131.044};
+
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 4,
+    center: myLatLng
+  });
+
+  var marker = new google.maps.Marker({
+    position: myLatLng,
+    map: map,
+    title: 'Hello World!'
+  });
+}
 </script>
 <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCQ1fUNExSmERLS6hUir3rCgFNp5DIlQss&callback=initMap"
         async defer></script>
